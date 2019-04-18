9f2a7feb12ed5d271202e2948d3aaa7ade999c69
homurax <homuraxenoblade@gmail.com>
2019-04-18 09:57:34 +0800

    add 122

---
+++ Algorithms/122. Best Time to Buy and Sell Stock II [Easy].java
@@ -0,0 +1,82 @@
+/**
+ * 122. Best Time to Buy and Sell Stock II
+ *
+ * Say you have an array for which the ith element is the price of a given stock on day i.
+ *
+ * Design an algorithm to find the maximum profit. You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times).
+ *
+ * Note: You may not engage in multiple transactions at the same time (i.e., you must sell the stock before you buy again).
+ *
+ * Example 1:
+ *
+ * Input: [7,1,5,3,6,4]
+ * Output: 7
+ * Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+ *              Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+ *
+ * Example 2:
+ *
+ * Input: [1,2,3,4,5]
+ * Output: 4
+ * Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
+ *              Note that you cannot buy on day 1, buy on day 2 and sell them later, as you are
+ *              engaging multiple transactions at the same time. You must sell before buying again.
+ *
+ * Example 3:
+ *
+ * Input: [7,6,4,3,1]
+ * Output: 0
+ * Explanation: In this case, no transaction is done, i.e. max profit = 0.
+ */
+public class BestTimeToBuyAndSellStockII {
+
+    /**
+     * 遍历找出每一次谷->峰
+     * 计算差值之和
+     */
+    public int maxProfit(int[] prices) {
+
+        if (prices == null || prices.length == 0) {
+            return 0;
+        }
+        int low = prices[0];
+        int high = prices[0];
+        int length = prices.length;
+        int i = 0;
+        int sum = 0;
+        while (i < length - 1) {
+
+            while (i < length - 1 && prices[i] >= prices[i + 1]) { // 谷底
+                i++;
+            }
+            low = prices[i];
+
+            while (i < length - 1 && prices[i] <= prices[i + 1]) { // 峰值
+                i++;
+            }
+            high = prices[i];
+            sum += high - low;
+        }
+        return sum;
+    }
+
+
+    /**
+     * 一个道理 从后往前看 计算差值之和
+     */
+    public int maxProfit2(int[] prices) {
+
+        if (prices == null || prices.length == 0) {
+            return 0;
+        }
+
+        int sum = 0;
+        int length = prices.length;
+        for (int i = 1; i < length; i++) {
+            if (prices[i] > prices[i - 1]) {
+                sum += prices[i] - prices[i - 1];
+            }
+        }
+        return sum;
+    }
+}
