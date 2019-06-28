4e557ea952f858b4d7d6e691a81da05fcecaa70e
homurax <homuraxenoblade@gmail.com>
2019-06-28 09:46:09 +0800

    189. Rotate Array [Easy]

---
+++ Algorithms/189. Rotate Array [Easy].java
@@ -0,0 +1,64 @@
+/**
+ * 189. Rotate Array
+ *
+ * Given an array, rotate the array to the right by k steps, where k is non-negative.
+ *
+ * Example 1:
+ *
+ * Input: [1,2,3,4,5,6,7] and k = 3
+ * Output: [5,6,7,1,2,3,4]
+ * Explanation:
+ * rotate 1 steps to the right: [7,1,2,3,4,5,6]
+ * rotate 2 steps to the right: [6,7,1,2,3,4,5]
+ * rotate 3 steps to the right: [5,6,7,1,2,3,4]
+ *
+ * Example 2:
+ *
+ * Input: [-1,-100,3,99] and k = 2
+ * Output: [3,99,-1,-100]
+ * Explanation:
+ * rotate 1 steps to the right: [99,-1,-100,3]
+ * rotate 2 steps to the right: [3,99,-1,-100]
+ *
+ * Note:
+ *
+ * Try to come up as many solutions as you can, there are at least 3 different ways to solve this problem.
+ * Could you do it in-place with O(1) extra space?
+ */
+public class RotateArray {
+
+    public void rotate(int[] nums, int k) {
+
+        k = k % nums.length;
+        if (k == 0) {
+            return;
+        }
+
+        int[] temp = new int[k];
+        System.arraycopy(nums, nums.length - k, temp, 0, k);
+        System.arraycopy(nums, 0, nums, k, nums.length - k);
+        System.arraycopy(temp, 0, nums, 0, k);
+    }
+
+
+    /**
+     * 整体反转一次后 两部分再各自反转一次即可
+     */
+    public void rotate2(int[] nums, int k) {
+        k = k % nums.length;
+
+        reverseRange(nums, 0, nums.length - 1);
+        reverseRange(nums, 0, k - 1);
+        reverseRange(nums, k, nums.length - 1);
+    }
+
+    private void reverseRange(int[] nums, int i, int j) {
+        while (i < j) {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++;
+            j--;
+        }
+    }
+}
