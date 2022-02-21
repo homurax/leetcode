164b74345cac90515eb9b5e2253d6bed3df7a7fc
homurax <homuraxenoblade@gmail.com>
2022-02-21 10:38:28 +0800

    add 304

---
+++ Algorithms/Java/medium/0304_Range_Sum_Query_2D_-_Immutable_[Medium].java
@@ -0,0 +1,68 @@
+/**
+ * 304. Range Sum Query 2D - Immutable
+ *
+ *
+ * Given a 2D matrix matrix, handle multiple queries of the following type:
+ *
+ * Calculate the sum of the elements of matrix inside the rectangle defined by its upper left corner (row1, col1) and lower right corner (row2, col2).
+ * Implement the NumMatrix class:
+ *
+ * NumMatrix(int[][] matrix) Initializes the object with the integer matrix matrix.
+ * int sumRegion(int row1, int col1, int row2, int col2) Returns the sum of the elements of matrix inside the rectangle defined by its upper left corner (row1, col1) and lower right corner (row2, col2).
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input
+ * ["NumMatrix", "sumRegion", "sumRegion", "sumRegion"]
+ * [[[[3, 0, 1, 4, 2], [5, 6, 3, 2, 1], [1, 2, 0, 1, 5], [4, 1, 0, 1, 7], [1, 0, 3, 0, 5]]], [2, 1, 4, 3], [1, 1, 2, 2], [1, 2, 2, 4]]
+ * Output
+ * [null, 8, 11, 12]
+ *
+ * Explanation
+ * NumMatrix numMatrix = new NumMatrix([[3, 0, 1, 4, 2], [5, 6, 3, 2, 1], [1, 2, 0, 1, 5], [4, 1, 0, 1, 7], [1, 0, 3, 0, 5]]);
+ * numMatrix.sumRegion(2, 1, 4, 3); // return 8 (i.e sum of the red rectangle)
+ * numMatrix.sumRegion(1, 1, 2, 2); // return 11 (i.e sum of the green rectangle)
+ * numMatrix.sumRegion(1, 2, 2, 4); // return 12 (i.e sum of the blue rectangle)
+ *
+ *
+ * Constraints:
+ *
+ * m == matrix.length
+ * n == matrix[i].length
+ * 1 <= m, n <= 200
+ * -105 <= matrix[i][j] <= 105
+ * 0 <= row1 <= row2 < m
+ * 0 <= col1 <= col2 < n
+ * At most 104 calls will be made to sumRegion.
+ */
+public class RangeSumQuery2DImmutable {
+
+    /**
+     * 二维前缀和
+     *
+     * p(i, j) = 以 (i， j) 为右下角的矩阵和
+     *
+     * P[i][j] = P[i - 1][j] + P[i][j - 1] - P[i - 1][j - 1] + matrix[i][j]
+     *         = 上部分       + 左部分       - 左上重复累加部分   + 当前值
+     */
+    class NumMatrix {
+
+        private int[][] preSum;
+
+        public NumMatrix(int[][] matrix) {
+            int m = matrix.length, n = matrix[0].length;
+            preSum = new int[m + 1][n + 1];
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    preSum[i + 1][j + 1] = preSum[i][j + 1] + preSum[i + 1][j] - preSum[i][j] + matrix[i][j];
+                }
+            }
+        }
+
+        public int sumRegion(int row1, int col1, int row2, int col2) {
+            return preSum[row2 + 1][col2 + 1] - preSum[row1][col2 + 1] - preSum[row2 + 1][col1] + preSum[row1][col1];
+        }
+    }
+}
