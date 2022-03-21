49908e534e99ec49d9729f74d7739b950e4fc454
homurax <homuraxenoblade@gmail.com>
2022-03-21 17:29:56 +0800

    add 1975

---
+++ Algorithms/Java/medium/1975_Maximum_Matrix_Sum_[Medium].java
@@ -0,0 +1,61 @@
+/**
+ * 1975. Maximum Matrix Sum
+ *
+ *
+ * You are given an n x n integer matrix. You can do the following operation any number of times:
+ *
+ * Choose any two adjacent elements of matrix and multiply each of them by -1.
+ * Two elements are considered adjacent if and only if they share a border.
+ *
+ * Your goal is to maximize the summation of the matrix's elements. Return the maximum sum of the matrix's elements using the operation mentioned above.
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: matrix = [[1,-1],[-1,1]]
+ * Output: 4
+ * Explanation: We can follow the following steps to reach sum equals 4:
+ * - Multiply the 2 elements in the first row by -1.
+ * - Multiply the 2 elements in the first column by -1.
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: matrix = [[1,2,3],[-1,-2,-3],[1,2,3]]
+ * Output: 16
+ * Explanation: We can follow the following step to reach sum equals 16:
+ * - Multiply the 2 last elements in the second row by -1.
+ *
+ *
+ * Constraints:
+ *
+ * 1. n == matrix.length == matrix[i].length
+ * 2. 2 <= n <= 250
+ * 3. -105 <= matrix[i][j] <= 10^5
+ */
+public class MaximumMatrixSum {
+
+    // 偶数个负数可以全变为正数
+    // 奇数个负数将最小值当作负数
+    public long maxMatrixSum(int[][] matrix) {
+        long sum = 0;
+        int min = Integer.MAX_VALUE, count = 0;
+        for (int[] ints : matrix) {
+            for (int i = 0; i < matrix.length; i++) {
+                sum += Math.abs(ints[i]);
+                if (ints[i] < 0) {
+                    count++;
+                }
+                min = Math.min(min, Math.abs(ints[i]));
+            }
+        }
+        if (count % 2 == 1) {
+            sum -= 2L * min;
+        }
+        return sum;
+    }
+
+}
--- README.md
+++ README.md
@@ -894,6 +894,7 @@ JDK 版本为 `1.8.0_144`。Python 版本为 `3.8.0`。Go 版本为 `1.17`。
 |1962|Remove Stones to Minimize the Total|[Java](/Algorithms/Java/medium/1962_Remove_Stones_to_Minimize_the_Total_[Medium].java)|Medium|
 |1963|Minimum Number of Swaps to Make the String Balanced|[Java](/Algorithms/Java/medium/1963_Minimum_Number_of_Swaps_to_Make_the_String_Balanced_[Medium].java)|Medium|
 |1968|Array With Elements Not Equal to Average of Neighbors|[Java](/Algorithms/Java/medium/1968_Array_With_Elements_Not_Equal_to_Average_of_Neighbors_[Medium].java)|Medium|
+|1975|Maximum Matrix Sum|[Java](/Algorithms/Java/medium/1975_Maximum_Matrix_Sum_[Medium].java)|Medium|
 |1980|Find Unique Binary String|[Java](/Algorithms/Java/medium/1980_Find_Unique_Binary_String_[Medium].java)|Medium|
 |1985|Find the Kth Largest Integer in the Array|[Go](/Algorithms/Go/medium/1985_Find_the_Kth_Largest_Integer_in_the_Array_[Medium].go), [Java](/Algorithms/Java/medium/1985_Find_the_Kth_Largest_Integer_in_the_Array_[Medium].java)|Medium|
 |1986|Minimum Number of Work Sessions to Finish the Tasks|[Java](/Algorithms/Java/medium/1986_Minimum_Number_of_Work_Sessions_to_Finish_the_Tasks_[Medium].java)|Medium|
