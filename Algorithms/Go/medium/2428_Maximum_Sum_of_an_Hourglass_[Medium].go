0324ec531b7992046a37d4e691932f20ea017890
homurax <homuraxenoblade@gmail.com>
2023-06-21 14:26:56 +0800

    add 2428

---
+++ Algorithms/Go/medium/2428_Maximum_Sum_of_an_Hourglass_[Medium].go
@@ -0,0 +1,47 @@
+/*
+2428. Maximum Sum of an Hourglass
+
+
+You are given an m x n integer matrix grid.
+
+We define an hourglass as a part of the matrix with the following form:
+
+
+Return the maximum sum of the elements of an hourglass.
+
+Note that an hourglass cannot be rotated and must be entirely contained within the matrix.
+
+
+
+Example 1:
+
+
+Input: grid = [[6,2,1,3],[4,2,1,5],[9,2,8,7],[4,1,2,9]]
+Output: 30
+Explanation: The cells shown above represent the hourglass with the maximum sum: 6 + 2 + 1 + 2 + 9 + 2 + 8 = 30.
+Example 2:
+
+
+Input: grid = [[1,2,3],[4,5,6],[7,8,9]]
+Output: 35
+Explanation: There is only one hourglass in the matrix, with the sum: 1 + 2 + 3 + 5 + 7 + 8 + 9 = 35.
+
+
+Constraints:
+
+1. m == grid.length
+2. n == grid[i].length
+3. 3 <= m, n <= 150
+4. 0 <= grid[i][j] <= 10^6
+*/
+
+func maxSum(grid [][]int) int {
+	m, n := len(grid), len(grid[0])
+	ans := 0
+	for i := 1; i < m-1; i++ {
+		for j := 0; j < n-1; j++ {
+			ans = max(ans, grid[i-1][j-1]+grid[i-1][j]+grid[i-1][j+1]+grid[i][j]+grid[i+1][j-1]+grid[i+1][j]+grid[i+1][j+1])
+		}
+	}
+	return ans
+}
