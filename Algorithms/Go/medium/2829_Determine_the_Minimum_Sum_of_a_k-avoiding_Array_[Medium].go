6c22550e513cc3d3903430519f3587dddabdf31c
homurax <homuraxenoblade@gmail.com>
2023-10-27 10:50:31 +0800

    add 2829

---
+++ Algorithms/Go/medium/2829_Determine_the_Minimum_Sum_of_a_k-avoiding_Array_[Medium].go
@@ -0,0 +1,44 @@
+/*
+2829. Determine the Minimum Sum of a k-avoiding Array
+
+
+You are given two integers, n and k.
+
+An array of distinct positive integers is called a k-avoiding array if there does not exist any pair of distinct elements that sum to k.
+
+Return the minimum possible sum of a k-avoiding array of length n.
+
+
+
+Example 1:
+
+Input: n = 5, k = 4
+Output: 18
+Explanation: Consider the k-avoiding array [1,2,4,5,6], which has a sum of 18.
+It can be proven that there is no k-avoiding array with a sum less than 18.
+
+
+Example 2:
+
+Input: n = 2, k = 6
+Output: 3
+Explanation: We can construct the array [1,2], which has a sum of 3.
+It can be proven that there is no k-avoiding array with a sum less than 3.
+
+
+Constraints:
+
+1 <= n, k <= 50
+*/
+
+// [1, k - 1] 只能选择 k / 2 个
+// 第一段, m = min(k / 2, n), (1 + m) * m / 2
+// 第二段选择 n - m 个数, [k, k + n - m - 1], (2 * k + n - m - 1) * (n - m) / 2
+// 当 k / 2 > n 时, m = n, 的第二段自然为 0
+func minimumSum(n int, k int) int {
+	m := k / 2
+	if m > n {
+		m = n
+	}
+	return ((1+m)*m + (k*2+n-m-1)*(n-m)) / 2
+}
