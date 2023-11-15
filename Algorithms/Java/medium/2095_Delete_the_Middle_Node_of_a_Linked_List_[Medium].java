3d86d736d90202ee5d458c754e1b1061e86085be
homurax <homuraxenoblade@gmail.com>
2023-11-15 10:44:06 +0800

    add 2095

---
+++ Algorithms/Java/medium/2095_Delete_the_Middle_Node_of_a_Linked_List_[Medium].java
@@ -0,0 +1,89 @@
+/**
+ * 2095. Delete the Middle Node of a Linked List
+ *
+ *
+ * You are given the head of a linked list. Delete the middle node, and return the head of the modified linked list.
+ *
+ * The middle node of a linked list of size n is the ⌊n / 2⌋th node from the start using 0-based indexing, where ⌊x⌋ denotes the largest integer less than or equal to x.
+ *
+ * For n = 1, 2, 3, 4, and 5, the middle nodes are 0, 1, 1, 2, and 2, respectively.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: head = [1,3,4,7,1,2,6]
+ * Output: [1,3,4,1,2,6]
+ * Explanation:
+ * The above figure represents the given linked list. The indices of the nodes are written below.
+ * Since n = 7, node 3 with value 7 is the middle node, which is marked in red.
+ * We return the new list after removing this node.
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: head = [1,2,3,4]
+ * Output: [1,2,4]
+ * Explanation:
+ * The above figure represents the given linked list.
+ * For n = 4, node 2 with value 3 is the middle node, which is marked in red.
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: head = [2,1]
+ * Output: [2]
+ * Explanation:
+ * The above figure represents the given linked list.
+ * For n = 2, node 1 with value 1 is the middle node, which is marked in red.
+ * Node 0 with value 2 is the only node remaining after removing node 1.
+ *
+ *
+ * Constraints:
+ *
+ * The number of nodes in the list is in the range [1, 10^5].
+ * 1 <= Node.val <= 10^5
+ */
+public class DeleteTheMiddleNodeOfALinkedList {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    // 或者记录慢指针的前一个节点
+    public ListNode deleteMiddle1(ListNode head) {
+        if (head.next == null) {
+            return null;
+        }
+        ListNode slow = head, fast = head, prev = null;
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        prev.next = prev.next.next;
+        return head;
+    }
+
+
+    // head 增加两个 dummy 节点
+    // 快慢指针法找到中间节点的前一个节点
+    public ListNode deleteMiddle(ListNode head) {
+        ListNode dummy = new ListNode(-1, new ListNode(-1));
+        dummy.next.next = head;
+        ListNode slow = dummy, fast = dummy;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        slow.next = slow.next.next;
+        return dummy.next.next;
+    }
+
+}
