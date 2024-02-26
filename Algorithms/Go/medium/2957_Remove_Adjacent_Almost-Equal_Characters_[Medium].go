507c0b78ad6677a4ae33d3f27c99bd8e14e8555c
homurax <homuraxenoblade@gmail.com>
2024-02-26 17:32:38 +0800

    add 2957

---
+++ Algorithms/Go/medium/2957_Remove_Adjacent_Almost-Equal_Characters_[Medium].go
@@ -0,0 +1,53 @@
+/*
+2957. Remove Adjacent Almost-Equal Characters
+
+
+You are given a 0-indexed string word.
+
+In one operation, you can pick any index i of word and change word[i] to any lowercase English letter.
+
+Return the minimum number of operations needed to remove all adjacent almost-equal characters from word.
+
+Two characters a and b are almost-equal if a == b or a and b are adjacent in the alphabet.
+
+
+
+Example 1:
+
+Input: word = "aaaaa"
+Output: 2
+Explanation: We can change word into "acaca" which does not have any adjacent almost-equal characters.
+It can be shown that the minimum number of operations needed to remove all adjacent almost-equal characters from word is 2.
+
+
+Example 2:
+
+Input: word = "abddez"
+Output: 2
+Explanation: We can change word into "ybdoez" which does not have any adjacent almost-equal characters.
+It can be shown that the minimum number of operations needed to remove all adjacent almost-equal characters from word is 2.
+
+
+Example 3:
+
+Input: word = "zyxyxyz"
+Output: 3
+Explanation: We can change word into "zaxaxaz" which does not have any adjacent almost-equal characters.
+It can be shown that the minimum number of operations needed to remove all adjacent almost-equal characters from word is 3.
+
+
+Constraints:
+
+1 <= word.length <= 100
+word consists only of lowercase English letters.
+*/
+func removeAlmostEqualCharacters(word string) int {
+	ans := 0
+	for i := 1; i < len(word); i++ {
+		if abs(int(word[i-1])-int(word[i])) <= 1 {
+			ans++
+			i++
+		}
+	}
+	return ans
+}
