8e8a4bb5d03a16caa851048d9a71cf003783b867
homurax <homuraxenoblade@gmail.com>
2024-04-10 10:31:39 +0800

    add 2962

---
+++ Algorithms/Go/medium/2962_Count_Subarrays_Where_Max_Element_Appears_at_Least_K_Times_[Medium].go
@@ -0,0 +1,53 @@
+/*
+2962. Count Subarrays Where Max Element Appears at Least K Times
+
+
+You are given an integer array nums and a positive integer k.
+
+Return the number of subarrays where the maximum element of nums appears at least k times in that subarray.
+
+A subarray is a contiguous sequence of elements within an array.
+
+
+
+Example 1:
+
+Input: nums = [1,3,2,3,3], k = 2
+Output: 6
+Explanation: The subarrays that contain the element 3 at least 2 times are: [1,3,2,3], [1,3,2,3,3], [3,2,3], [3,2,3,3], [2,3,3] and [3,3].
+
+
+Example 2:
+
+Input: nums = [1,4,2,1], k = 3
+Output: 0
+Explanation: No subarray contains the element 4 at least 3 times.
+
+
+Constraints:
+
+1 <= nums.length <= 10^5
+1 <= nums[i] <= 10^6
+1 <= k <= 10^5
+*/
+
+func countSubarrays(nums []int, k int) int64 {
+	mx := 0
+	for _, num := range nums {
+		mx = max(mx, num)
+	}
+	cntMx, left, ans := 0, 0, 0
+	for _, x := range nums {
+		if x == mx {
+			cntMx++
+		}
+		for cntMx == k {
+			if nums[left] == mx {
+				cntMx--
+			}
+			left++
+		}
+		ans += left
+	}
+	return int64(ans)
+}
