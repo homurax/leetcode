732fc16453aeb50597cfcb37da172aab78b0f05d
homurax <homuraxenoblade@gmail.com>
2024-05-13 13:35:54 +0800

    add 3084

---
+++ Algorithms/Go/medium/3084_Count_Substrings_Starting_and_Ending_with_Given_Character_[Medium].go
@@ -0,0 +1,36 @@
+/*
+3084. Count Substrings Starting and Ending with Given Character
+
+
+You are given a string s and a character c. Return the total number of substrings of s that start and end with c.
+
+
+
+Example 1:
+
+Input: s = "abada", c = "a"
+
+Output: 6
+
+Explanation: Substrings starting and ending with "a" are: "abada", "abada", "abada", "abada", "abada", "abada".
+
+
+Example 2:
+
+Input: s = "zzz", c = "z"
+
+Output: 6
+
+Explanation: There are a total of 6 substrings in s and all start and end with "z".
+
+
+
+Constraints:
+
+1 <= s.length <= 10^5
+s and c consist only of lowercase English letters.
+*/
+func countSubstrings(s string, c byte) int64 {
+	k := int64(strings.Count(s, string(c)))
+	return k * (k + 1) / 2
+}
