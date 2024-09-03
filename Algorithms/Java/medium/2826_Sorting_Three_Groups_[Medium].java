2ae08b80527ad57d49ca24ceef9961d0aafdc31d
homurax <homuraxenoblade@gmail.com>
2024-09-03 10:28:57 +0800

    add 2826

---
+++ Algorithms/Java/medium/2826_Sorting_Three_Groups_[Medium].java
@@ -0,0 +1,59 @@
+/**
+ * 2826. Sorting Three Groups
+ *
+ *
+ * You are given an integer array nums. Each element in nums is 1, 2 or 3. In each operation, you can remove an element from nums. Return the minimum number of operations to make nums non-decreasing.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: nums = [2,1,3,2,1]
+ *
+ * Output: 3
+ *
+ * Explanation:
+ *
+ * One of the optimal solutions is to remove nums[0], nums[2] and nums[3].
+ *
+ * Example 2:
+ *
+ * Input: nums = [1,3,2,1,3,3]
+ *
+ * Output: 2
+ *
+ * Explanation:
+ *
+ * One of the optimal solutions is to remove nums[1] and nums[2].
+ *
+ * Example 3:
+ *
+ * Input: nums = [2,2,2,2,3,3]
+ *
+ * Output: 0
+ *
+ * Explanation:
+ *
+ * nums is already non-decreasing.
+ *
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= nums.length <= 100
+ * 1 <= nums[i] <= 3
+ */
+public class SortingThreeGroups {
+
+    // 考虑最多需要保留的
+    public int minimumOperations(List<Integer> nums) {
+        int[] cnt = new int[4];
+        for (Integer num : nums) {
+            cnt[num]++;
+            cnt[2] = Math.max(cnt[1], cnt[2]);
+            cnt[3] = Math.max(cnt[2], cnt[3]);
+        }
+        return nums.size() - cnt[3];
+    }
+
+}
