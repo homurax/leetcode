38056981d1a8c2e9416e819b93a3ef2397323408
homurax <homuraxenoblade@gmail.com>
2024-09-29 10:13:57 +0800

    add 2587

---
+++ Algorithms/Go/medium/2587_Rearrange_Array_to_Maximize_Prefix_Score_[Medium].go
@@ -0,0 +1,39 @@
+/*
+2587. Rearrange Array to Maximize Prefix Score
+
+You are given a 0-indexed integer array nums. You can rearrange the elements of nums to any order (including the given order).
+
+Let prefix be the array containing the prefix sums of nums after rearranging it. In other words, prefix[i] is the sum of the elements from 0 to i in nums after rearranging it. The score of nums is the number of positive integers in the array prefix.
+
+Return the maximum score you can achieve.
+
+Example 1:
+
+Input: nums = [2,-1,0,1,-3,3,-3]
+Output: 6
+Explanation: We can rearrange the array into nums = [2,3,1,-1,-3,0,-3].
+prefix = [2,5,6,5,2,2,-1], so the score is 6.
+It can be shown that 6 is the maximum score we can obtain.
+Example 2:
+
+Input: nums = [-2,-3,0]
+Output: 0
+Explanation: Any rearrangement of the array will result in a score of 0.
+
+Constraints:
+
+1 <= nums.length <= 10^5
+-10^6 <= nums[i] <= 10^6
+*/
+func maxScore(nums []int) int {
+	sort.Sort(sort.Reverse(sort.IntSlice(nums)))
+	sum, ans := 0, 0
+	for _, v := range nums {
+		sum += v
+		if sum <= 0 {
+			break
+		}
+		ans++
+	}
+	return ans
+}
