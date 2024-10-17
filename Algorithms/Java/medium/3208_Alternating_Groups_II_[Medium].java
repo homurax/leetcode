ff22daae2c8c53fdbbfbb8b71f6f2f3b2cf5a3e0
homurax <homuraxenoblade@gmail.com>
2024-10-17 10:53:07 +0800

    add 3208

---
+++ Algorithms/Java/medium/3208_Alternating_Groups_II_[Medium].java
@@ -0,0 +1,82 @@
+/**
+ * 3208. Alternating Groups II
+ *
+ *
+ * There is a circle of red and blue tiles. You are given an array of integers colors and an integer k. The color of tile i is represented by colors[i]:
+ *
+ * colors[i] == 0 means that tile i is red.
+ * colors[i] == 1 means that tile i is blue.
+ * An alternating group is every k contiguous tiles in the circle with alternating colors (each tile in the group except the first and last one has a different color from its left and right tiles).
+ *
+ * Return the number of alternating groups.
+ *
+ * Note that since colors represents a circle, the first and the last tiles are considered to be next to each other.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: colors = [0,1,0,1,0], k = 3
+ *
+ * Output: 3
+ *
+ * Explanation:
+ *
+ *
+ *
+ * Alternating groups:
+ *
+ *
+ *
+ * Example 2:
+ *
+ * Input: colors = [0,1,0,0,1,0,1], k = 6
+ *
+ * Output: 2
+ *
+ * Explanation:
+ *
+ *
+ *
+ * Alternating groups:
+ *
+ *
+ *
+ * Example 3:
+ *
+ * Input: colors = [1,1,0,1], k = 4
+ *
+ * Output: 0
+ *
+ * Explanation:
+ *
+ *
+ *
+ *
+ *
+ * Constraints:
+ *
+ * 3 <= colors.length <= 10^5
+ * 0 <= colors[i] <= 1
+ * 3 <= k <= colors.length
+ */
+public class AlternatingGroupsII {
+
+    // 复制一份拼接 -> colors[i % n]
+    // 计算 i >= n 部分批量重复统计
+    public int numberOfAlternatingGroups(int[] colors, int k) {
+        int n = colors.length;
+        int ans = 0, cnt = 0;
+        for (int i = 0; i < n * 2; i++) {
+            if (i > 0 && colors[i % n] == colors[(i - 1) % n]) {
+                cnt = 0;
+            }
+            cnt++;
+            if (i >= n && cnt >= k) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+
+}
