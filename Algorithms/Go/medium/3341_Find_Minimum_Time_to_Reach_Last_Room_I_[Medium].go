403cdf9d1a1dfdcd83004765c66e124a0aab1d95
homurax <homuraxenoblade@gmail.com>
2025-03-03 10:34:32 +0800

    add 3341

---
+++ Algorithms/Go/medium/3341_Find_Minimum_Time_to_Reach_Last_Room_I_[Medium].go
@@ -0,0 +1,91 @@
+/*
+3341. Find Minimum Time to Reach Last Room I
+
+There is a dungeon with n x m rooms arranged as a grid.
+
+You are given a 2D array moveTime of size n x m, where moveTime[i][j] represents the minimum time in seconds when you can start moving to that room. You start from the room (0, 0) at time t = 0 and can move to an adjacent room. Moving between adjacent rooms takes exactly one second.
+
+Return the minimum time to reach the room (n - 1, m - 1).
+
+Two rooms are adjacent if they share a common wall, either horizontally or vertically.
+
+Example 1:
+
+Input: moveTime = [[0,4],[4,4]]
+
+Output: 6
+
+Explanation:
+
+The minimum time required is 6 seconds.
+
+At time t == 4, move from room (0, 0) to room (1, 0) in one second.
+At time t == 5, move from room (1, 0) to room (1, 1) in one second.
+Example 2:
+
+Input: moveTime = [[0,0,0],[0,0,0]]
+
+Output: 3
+
+Explanation:
+
+The minimum time required is 3 seconds.
+
+At time t == 0, move from room (0, 0) to room (1, 0) in one second.
+At time t == 1, move from room (1, 0) to room (1, 1) in one second.
+At time t == 2, move from room (1, 1) to room (1, 2) in one second.
+Example 3:
+
+Input: moveTime = [[0,1],[1,2]]
+
+Output: 3
+
+Constraints:
+
+2 <= n == moveTime.length <= 50
+2 <= m == moveTime[i].length <= 50
+0 <= moveTime[i][j] <= 10^9
+*/
+var dirs = []struct{ x, y int }{{-1, 0}, {1, 0}, {0, -1}, {0, 1}}
+
+func minTimeToReach(moveTime [][]int) (ans int) {
+	n, m := len(moveTime), len(moveTime[0])
+	dis := make([][]int, n)
+	for i := range dis {
+		dis[i] = make([]int, m)
+		for j := range dis[i] {
+			dis[i][j] = math.MaxInt
+		}
+	}
+	dis[0][0] = 0
+	h := hp{{}}
+	for {
+		top := heap.Pop(&h).(tuple)
+		i, j := top.x, top.y
+		if i == n-1 && j == m-1 {
+			return top.dis
+		}
+		if top.dis > dis[i][j] {
+			continue
+		}
+		for _, d := range dirs {
+			x, y := i+d.x, j+d.y
+			if 0 <= x && x < n && 0 <= y && y < m {
+				newD := max(top.dis, moveTime[x][y]) + 1
+				if newD < dis[x][y] {
+					dis[x][y] = newD
+					heap.Push(&h, tuple{newD, x, y})
+				}
+			}
+		}
+	}
+}
+
+type tuple struct{ dis, x, y int }
+type hp []tuple
+
+func (h hp) Len() int           { return len(h) }
+func (h hp) Less(i, j int) bool { return h[i].dis < h[j].dis }
+func (h hp) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+func (h *hp) Push(v any)        { *h = append(*h, v.(tuple)) }
+func (h *hp) Pop() (v any)      { a := *h; *h, v = a[:len(a)-1], a[len(a)-1]; return }
