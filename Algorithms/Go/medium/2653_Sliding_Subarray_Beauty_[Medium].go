0d45f890f86238300a34c5c58189236de975fd10
homurax <homuraxenoblade@gmail.com>
2025-03-26 10:41:41 +0800

    add 2653

---
+++ Algorithms/Go/medium/2653_Sliding_Subarray_Beauty_[Medium].go
@@ -0,0 +1,83 @@
+/*
+2653. Sliding Subarray Beauty
+
+
+Given an integer array nums containing n integers, find the beauty of each subarray of size k.
+
+The beauty of a subarray is the xth smallest integer in the subarray if it is negative, or 0 if there are fewer than x negative integers.
+
+Return an integer array containing n - k + 1 integers, which denote the beauty of the subarrays in order from the first index in the array.
+
+A subarray is a contiguous non-empty sequence of elements within an array.
+
+
+
+Example 1:
+
+Input: nums = [1,-1,-3,-2,3], k = 3, x = 2
+Output: [-1,-2,-2]
+Explanation: There are 3 subarrays with size k = 3.
+The first subarray is [1, -1, -3] and the 2nd smallest negative integer is -1.
+The second subarray is [-1, -3, -2] and the 2nd smallest negative integer is -2.
+The third subarray is [-3, -2, 3] and the 2nd smallest negative integer is -2.
+
+
+Example 2:
+
+Input: nums = [-1,-2,-3,-4,-5], k = 2, x = 2
+Output: [-1,-2,-3,-4]
+Explanation: There are 4 subarrays with size k = 2.
+For [-1, -2], the 2nd smallest negative integer is -1.
+For [-2, -3], the 2nd smallest negative integer is -2.
+For [-3, -4], the 2nd smallest negative integer is -3.
+For [-4, -5], the 2nd smallest negative integer is -4.
+
+
+Example 3:
+
+Input: nums = [-3,1,2,-3,0,-3], k = 2, x = 1
+Output: [-3,0,-3,-3,-3]
+Explanation: There are 5 subarrays with size k = 2.
+For [-3, 1], the 1st smallest negative integer is -3.
+For [1, 2], there is no negative integer so the beauty is 0.
+For [2, -3], the 1st smallest negative integer is -3.
+For [-3, 0], the 1st smallest negative integer is -3.
+For [0, -3], the 1st smallest negative integer is -3.
+
+
+Constraints:
+
+n == nums.length
+1 <= n <= 10^5
+1 <= k <= n
+1 <= x <= k
+-50 <= nums[i] <= 50
+*/
+
+// 范围固定
+// 滑窗 + 计数排序
+// 第 x 小: 设它是 num, 那么 < num 的数有 < x 个, ≤ num 的数有 ≥ x 个, 就说明 num 是第 x 小的数
+func getSubarrayBeauty(nums []int, k int, x int) []int {
+	const bias = 50
+	cnt := [bias*2 + 1]int{}
+	for _, num := range nums[:k-1] { // 准备窗口
+		cnt[num+bias]++
+	}
+
+	ans := make([]int, len(nums)-k+1)
+	for i, num := range nums[k-1:] {
+		// 进入窗口
+		cnt[num+bias]++
+		left := x
+		for j, c := range cnt[:bias] { // 负数范围 [-50,-1]
+			left -= c
+			if left <= 0 { // 第 x 小
+				ans[i] = j - bias
+				break
+			}
+		}
+		// 离开窗口
+		cnt[nums[i]+bias]--
+	}
+	return ans
+}
