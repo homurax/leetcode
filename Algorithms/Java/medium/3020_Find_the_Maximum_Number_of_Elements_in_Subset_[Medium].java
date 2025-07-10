c94de700402f4dda9105f9d67540db333054c194
homurax <homuraxenoblade@gmail.com>
2025-07-10 15:00:29 +0800

    add 3020

---
+++ Algorithms/Java/medium/3020_Find_the_Maximum_Number_of_Elements_in_Subset_[Medium].java
@@ -0,0 +1,57 @@
+/**
+ * 3020. Find the Maximum Number of Elements in Subset
+ *
+ *
+ * You are given an array of positive integers nums.
+ *
+ * You need to select a subset of nums which satisfies the following condition:
+ *
+ * You can place the selected elements in a 0-indexed array such that it follows the pattern: [x, x2, x4, ..., xk/2, xk, xk/2, ..., x4, x2, x] (Note that k can be be any non-negative power of 2). For example, [2, 4, 16, 4, 2] and [3, 9, 3] follow the pattern while [2, 4, 8, 4, 2] does not.
+ * Return the maximum number of elements in a subset that satisfies these conditions.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: nums = [5,4,1,2,2]
+ * Output: 3
+ * Explanation: We can select the subset {4,2,2}, which can be placed in the array as [2,4,2] which follows the pattern and 22 == 4. Hence the answer is 3.
+ *
+ *
+ * Example 2:
+ *
+ * Input: nums = [1,3,2,4]
+ * Output: 1
+ * Explanation: We can select the subset {1}, which can be placed in the array as [1] which follows the pattern. Hence the answer is 1. Note that we could have also selected the subsets {2}, {3}, or {4}, there may be multiple subsets which provide the same answer.
+ *
+ *
+ * Constraints:
+ *
+ * 2 <= nums.length <= 10^5
+ * 1 <= nums[i] <= 10^9
+ */
+public class FindTheMaximumNumberOfElementsInSubset {
+
+    // x^2p <= max(nums)
+    // p <= log2logx^m -> p <= 4
+    public int maximumLength(int[] nums) {
+        Map<Long, Integer> cnt = new HashMap<>();
+        for (int x : nums) {
+            cnt.merge((long) x, 1, Integer::sum);
+        }
+        Integer c1 = cnt.remove(1L);
+        // 特殊考虑全是 1 的情况，确保为奇数个
+        int ans = c1 != null ? c1 - 1 | 1 : 0;
+        for (long x : cnt.keySet()) {
+            int res = 0;
+            for (; cnt.getOrDefault(x, 0) > 1; x *= x) {
+                res += 2;
+            }
+            // x 存在，x 就是最大值，个数 +1
+            // x 不存在，前一个 x 是最大值，需要 -1，只取一个
+            ans = Math.max(ans, res + (cnt.containsKey(x) ? 1 : -1));
+        }
+        return ans;
+    }
+
+}
